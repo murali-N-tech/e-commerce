36063d143cbf014d7bcf9c4453db3f9def7ee04b
murali-N-tech <muralinaga826@gmail.com>
2025-06-25 14:16:34 +0530

    Update orderRoutes.js

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -7,24 +7,19 @@ const {
   getOrderById,
   updateOrderToPaid,
   getMyOrders,
-  getOrders,         // For admin
-  updateOrderToDelivered, // For admin
+  getOrders,
+  updateOrderToDelivered,
 } = require('../controllers/orderController');
 const { protect } = require('../middleware/authMiddleware');
-const { admin } = require('../middleware/adminMiddleware'); // Import admin middleware
+const { admin } = require('../middleware/adminMiddleware');
 
-// Routes for creating and fetching all orders (admin only)
 router.route('/')
-  .post(protect, addOrderItems) // Create a new order (private, for logged-in user)
-  .get(protect, admin, getOrders); // Get all orders (private, admin only)
+  .post(protect, addOrderItems)
+  .get(protect, admin, getOrders);
 
-// Route for getting logged in user's orders
 router.route('/myorders').get(protect, getMyOrders);
-
-// Routes for specific order by ID
-router.route('/:id').get(protect, getOrderById); // Get single order by ID (private, user or admin)
-router.route('/:id/pay').put(protect, updateOrderToPaid); // Update order to paid (private)
-router.route('/:id/deliver').put(protect, admin, updateOrderToDelivered); // Update order to delivered (private, admin only)
-
+router.route('/:id').get(protect, getOrderById);
+router.route('/:id/pay').put(protect, updateOrderToPaid);
+router.route('/:id/deliver').put(protect, admin, updateOrderToDelivered);
 
 module.exports = router;
